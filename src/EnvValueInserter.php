fd5bdf90ebe9b8b056ce4790d1146482517d6f20
Patrick Morpey <patrick@morphp.nl>
2022-07-18 13:55:36 +0200

    If the string as a whole refers to one ENV variable, and that one isn't set, return null

--- src/EnvValueInserter.php
+++ src/EnvValueInserter.php
@@ -10,8 +10,14 @@ use Medas\ServiceManager\Attributes\Service;
 #[Service]
 class EnvValueInserter
 {
-    public function insert(string $value): string
+    public function insert(string $value): string|null
     {
+        if (preg_match('/^\$env\((\w+)\)$/', $value, $match)) {
+            // If the string as a whole refers to one ENV variable, and that one isn't set,
+            // return null
+            return array_key_exists($match[1], $_ENV) ? $_ENV[$match[1]] : null;
+        }
+
         if (preg_match_all('/\$env\((\w+)\)/', $value, $matches, PREG_SET_ORDER)) {
             foreach ($matches as $match) {
                 if (!array_key_exists($match[1], $_ENV)) {
