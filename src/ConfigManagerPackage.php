60e5780de31be4b8798039902d91ae51dfc7574f
Patrick Morpey <patrick@morphp.nl>
2023-08-16 16:16:54 +0200

    Updated dependencies

--- src/ConfigManagerPackage.php
+++ src/ConfigManagerPackage.php
@@ -13,8 +13,7 @@ class ConfigManagerPackage extends BasePackage
 
     public function dependencies(): array
     {
-        return $this->dependenciesByClass([
-        ]);
+        return [];
     }
 
     public function sourceDirectory(): string
