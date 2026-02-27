c0eab1a4ab10cd40bd30cd7d8c3b99eca6dd43a3
Patrick Morpey <patrick@morphp.nl>
2026-02-27 12:59:07 +0100

    Updated dependencies

--- src/ConfigManagerPackage.php
+++ src/ConfigManagerPackage.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\ConfigManager;
 
-use Medas\Core\AsSingleton;
-use Medas\ServiceManager\BasePackage;
+use Medas\Core\{AsSingleton, BasePackage};
 
 class ConfigManagerPackage extends BasePackage
 {
